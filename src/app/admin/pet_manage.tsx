4c408ebefcee23a28ddb1eb57d0e3f2db181f435
Belle <bellebt2019@gmail.com>
2024-09-26 14:20:18 +0700

    remove extras

--- src/app/admin/pet_manage.tsx
+++ src/app/admin/pet_manage.tsx
@@ -24,10 +24,8 @@ import { Icon } from "@iconify/react";
 const columns = [
   { name: "ID", uid: "id", sortable: true },
   { name: "NAME", uid: "name", sortable: true },
-  { name: "AGE", uid: "age", sortable: true },
-  { name: "ROLE", uid: "role", sortable: true },
-  { name: "TEAM", uid: "team" },
-  { name: "EMAIL", uid: "email" },
+  { name: "BIRTH DATE", uid: "age", sortable: true },
+  { name: "SPECIES", uid: "role", sortable: true },
   { name: "STATUS", uid: "status", sortable: true },
   { name: "ACTIONS", uid: "actions" },
 ];
