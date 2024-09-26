a4cc2ab33f19efb9d35e7f411013c0ef9215d825
Belle <bellebt2019@gmail.com>
2024-09-26 10:57:26 +0700

    Admin pet management

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Pet_manage from "./pet_manage";
+import Menu from "./menu";
 
 export default function page() {
-  return <div>p</div>;
+  return (
+    <div className="flex">
+      <Menu />
+      <Pet_manage />
+    </div>
+  );
 }
