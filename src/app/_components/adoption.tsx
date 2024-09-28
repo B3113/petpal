883792437c8010b3a05e487528115ba6e109e48b
Belle <bellebt2019@gmail.com>
2024-09-28 15:49:32 +0700

    transfer tab and search to adoption

--- src/app/_components/adoption.tsx
+++ src/app/_components/adoption.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Card,
   CardBody,
@@ -16,10 +16,14 @@ import {
   useDisclosure,
   Input,
   DateInput,
+  Tabs,
+  Tab,
 } from "@nextui-org/react";
 import { api } from "~/trpc/react";
 import { CalendarDate } from "@internationalized/date";
 import { type Session } from "next-auth";
+import { Icon } from "@iconify/react";
+import { Pet } from "@prisma/client";
 
 const statusColorMap: Record<string, ChipProps["color"]> = {
   available: "success",
@@ -32,9 +36,10 @@ type Props = {
 };
 
 export default function Adoption({ session }: Props) {
+  const [selected, setSelected] = React.useState<React.Key>("all");
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [selectedPet, setSelectedPet] = React.useState("");
-  const pets = api.pet.get.useQuery().data;
+  const pets = api.pet.get.useQuery().data ?? [];
   console.log(pets);
   console.log(session);
 
@@ -62,10 +67,57 @@ export default function Adoption({ session }: Props) {
     });
   };
 
+  const [data, setData] = React.useState<Pet[]>(pets);
+
+  useEffect(() => {
+    setData(pets);
+  }, [pets]);
+
+  const filterPets = (key: React.Key) => {
+    if (key === "all") {
+      setData(pets);
+    } else {
+      setData(pets.filter((pet) => pet.specie === key));
+    }
+  };
+
   return (
-    <div id="adoption" className="mt-20 flex justify-center">
-      <div className="grid h-full w-[300px] gap-10 md:w-[600px] md:grid-cols-2 lg:w-[900px] lg:grid-cols-3 lg:gap-5">
-        {pets?.map((item, index) => (
+    <div
+      id="adoption"
+      className="mt-10 flex flex-col items-center justify-center"
+    >
+      <div>
+        <div className="mt-8 flex gap-8 lg:mt-5">
+          <Input
+            isClearable
+            radius="full"
+            placeholder="Search..."
+            onValueChange={(value) => {
+              setData(
+                pets.filter((pet) =>
+                  pet.name.toLowerCase().includes(value.toLowerCase()),
+                ),
+              );
+            }}
+            className="w-[200px] md:w-[350px] xl:w-[380px]"
+          />
+        </div>
+
+        <Tabs
+          radius="full"
+          variant="light"
+          className="mt-5 gap-4 opacity-60"
+          onSelectionChange={filterPets}
+        >
+          <Tab title="All" key={"all"}></Tab>
+          <Tab title="Dog" key={"dog"}></Tab>
+          <Tab title="Cat" key={"cat"}></Tab>
+          <Tab title="Rabbit" key={"rabbit"}></Tab>
+        </Tabs>
+      </div>
+
+      <div className="mt-10 grid h-full w-[300px] gap-10 md:w-[600px] md:grid-cols-2 lg:w-[900px] lg:grid-cols-3 lg:gap-5">
+        {data?.map((item, index) => (
           <div className="relative" key={index}>
             <Card shadow="sm" isPressable>
               <CardBody className="overflow-visible p-0">
@@ -106,66 +158,66 @@ export default function Adoption({ session }: Props) {
                 <div className="mt-1 text-sm">{item.description}</div>
               </div>
             </div>
-            <Modal
-              backdrop="opaque"
-              isOpen={isOpen}
-              onOpenChange={onOpenChange}
-              radius="lg"
-              classNames={{
-                body: "py-6",
-                backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
-                base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
-                header: "border-b-[1px] border-[#292f46]",
-                footer: "border-t-[1px] border-[#292f46]",
-                closeButton: "hover:bg-white/5 active:bg-white/10",
-              }}
-            >
-              <ModalContent>
-                {(onClose) => (
-                  <form action={handleAdopt}>
-                    <ModalHeader className="flex flex-col gap-1">
-                      Adoption form
-                    </ModalHeader>
-                    <ModalBody>
-                      <Input
-                        name="name"
-                        variant="bordered"
-                        type="Name"
-                        label="Name"
-                      />
-                      <Input
-                        name="career"
-                        variant="bordered"
-                        type="Career"
-                        label="Career"
-                      />
-                      <DateInput
-                        variant="bordered"
-                        label={"Birth date"}
-                        placeholderValue={new CalendarDate(1995, 11, 6)}
-                        className="w-full"
-                        name="birthdate"
-                      />
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button className="text-[#6f4ef2]" onPress={onClose}>
-                        Close
-                      </Button>
-                      <Button
-                        className="bg-[#6f4ef2] shadow-lg shadow-indigo-500/20"
-                        onPress={onClose}
-                        type="submit"
-                      >
-                        Adopt
-                      </Button>
-                    </ModalFooter>
-                  </form>
-                )}
-              </ModalContent>
-            </Modal>
           </div>
         ))}
       </div>
+      <Modal
+        backdrop="opaque"
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        radius="lg"
+        classNames={{
+          body: "py-6",
+          backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
+          base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
+          header: "border-b-[1px] border-[#292f46]",
+          footer: "border-t-[1px] border-[#292f46]",
+          closeButton: "hover:bg-white/5 active:bg-white/10",
+        }}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <form action={handleAdopt}>
+              <ModalHeader className="flex flex-col gap-1">
+                Adoption form
+              </ModalHeader>
+              <ModalBody>
+                <Input
+                  name="name"
+                  variant="bordered"
+                  type="Name"
+                  label="Name"
+                />
+                <Input
+                  name="career"
+                  variant="bordered"
+                  type="Career"
+                  label="Career"
+                />
+                <DateInput
+                  variant="bordered"
+                  label={"Birth date"}
+                  placeholderValue={new CalendarDate(1995, 11, 6)}
+                  className="w-full"
+                  name="birthdate"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button className="text-[#6f4ef2]" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  className="bg-[#6f4ef2] shadow-lg shadow-indigo-500/20"
+                  onPress={onClose}
+                  type="submit"
+                >
+                  Adopt
+                </Button>
+              </ModalFooter>
+            </form>
+          )}
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
--- src/app/_components/home.tsx
+++ src/app/_components/home.tsx
@@ -14,7 +14,7 @@ import { Icon } from "@iconify/react";
 export default function Home() {
   return (
     <div id="home">
-      <div className="mt-20 flex flex-col-reverse items-center justify-between px-0 md:px-16 lg:flex-row lg:px-32 xl:px-56">
+      <div className="my-20 flex flex-col-reverse items-center justify-between px-0 md:px-16 lg:flex-row lg:px-32 xl:px-56">
         <div className="mt-10 flex flex-col items-center lg:mt-0 lg:items-start">
           <div className="pointer-events-none flex h-6 w-40 items-center gap-2 rounded-full border border-black/20 px-5 text-[10px] text-black/60 lg:h-8 lg:w-44 lg:text-xs">
             <Icon icon="ph:paw-print-fill" />
@@ -27,24 +27,6 @@ export default function Home() {
             Discover your perfect companion and give <br /> a pet in need a
             forever home.
           </div>
-          <div className="mt-8 flex items-center gap-8 lg:mt-5">
-            <Input
-              isClearable
-              radius="full"
-              placeholder="Search..."
-              className="w-[200px] md:w-[350px] xl:w-[380px]"
-            />
-            <Button color="secondary" variant="flat">
-              <Icon icon="line-md:search" />
-            </Button>
-          </div>
-
-          <Tabs radius="full" variant="light" className="mt-5 gap-4 opacity-60">
-            <Tab title="All"></Tab>
-            <Tab title="Dog"></Tab>
-            <Tab title="Cat"></Tab>
-            <Tab title="Rabbit"></Tab>
-          </Tabs>
         </div>
         <div className="felx felx-col items-center justify-center">
           <Card
@@ -53,37 +35,7 @@ export default function Home() {
             className="h-[200px] w-[200px] border-none md:h-[250px] md:w-[250px] lg:h-[300px] lg:w-[300px] xl:h-[340px] xl:w-[340px]"
           >
             <Image alt="Pets" className="object-cover" src="../img/dog.png" />
-            <CardFooter className="absolute bottom-1 z-10 ml-1 w-[calc(100%_-_8px)] justify-between overflow-hidden rounded-large border-1 border-white/20 py-1 shadow-small before:rounded-xl before:bg-white/10">
-              <p className="pointer-events-none text-tiny text-black/80">
-                Dalmatian
-              </p>
-              <Button
-                className="bg-black/20 text-tiny text-white"
-                variant="flat"
-                color="default"
-                radius="lg"
-                size="sm"
-              >
-                Adopt
-              </Button>
-            </CardFooter>
           </Card>
-          <div className="mt-5 flex items-center justify-center gap-5">
-            <Button
-              variant="light"
-              size="sm"
-              className="opacity-60 hover:opacity-100"
-            >
-              <Icon icon="fe:arrow-left" />
-            </Button>
-            <Button
-              variant="light"
-              size="sm"
-              className="opacity-60 hover:opacity-100"
-            >
-              <Icon icon="fe:arrow-right" />
-            </Button>
-          </div>
         </div>
       </div>
       <hr className="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700"></hr>
